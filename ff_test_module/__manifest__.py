915ef7281b814fb14ae01a90563ea7f5e9c5de1d
fabio <fabio.facundo@giordano.ch>
2025-01-31 10:21:48 +0100

    [FIX] fixed css

--- ff_test_module/__manifest__.py
+++ ff_test_module/__manifest__.py
@@ -10,11 +10,11 @@
 
     'assets': {
         'web.report_assets_common': [
-            'ff_test_module/static/src/scss/my_model.scss'
-        ],
-        'web.assets_backend': [
+            'ff_test_module/static/src/scss/my_model.scss',
             'ff_test_module/static/src/scss/style.css'
         ]
+        # 'web.assets_backend': [
+        # ]
     },
     'data': [
         'security/ir.model.access.csv',
